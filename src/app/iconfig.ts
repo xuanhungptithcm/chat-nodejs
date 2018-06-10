ec0d51f2c3af0d0bec73dbd07bf2d2caf175c528
GTS-IT-GROUP-2018 <gtsit.secure@gmail.com>
2018-06-10 20:43:37 +0700

    8:43PM

--- src/app/iconfig.ts
+++ src/app/iconfig.ts
@@ -1,26 +1,26 @@
-// export const Configs = {
-//     USER: {
-//         register: 'http://localhost:' + '3000/api/user/resgister',
-//         login: 'http://localhost:' + '3000/api/auth/login',
-//         token: 'http://localhost:' + '3000/api/auth/getuserbytoken',
-//         update: 'http://localhost:' + '3000/api/user',
-//         uploadfile: 'http://localhost:' + '3000/api/user/uploadfile',
-//     },
-//     SOCKET: {
-//         url: 'http://localhost:3000'
-//     }
-// };
-
 export const Configs = {
     USER: {
-        register: 'https://nodejs-server-chat.herokuapp.com/' + 'api/user/resgister',
-        login: 'https://nodejs-server-chat.herokuapp.com/' + 'api/auth/login',
-        token: 'https://nodejs-server-chat.herokuapp.com/' + 'api/auth/getuserbytoken',
-        update: 'https://nodejs-server-chat.herokuapp.com/' + 'api/user',
-        uploadfile: 'https://nodejs-server-chat.herokuapp.com/' + 'api/user/uploadfile',
+        register: 'http://localhost:' + '3000/api/user/resgister',
+        login: 'http://localhost:' + '3000/api/auth/login',
+        token: 'http://localhost:' + '3000/api/auth/getuserbytoken',
+        update: 'http://localhost:' + '3000/api/user',
+        uploadfile: 'http://localhost:' + '3000/api/user/uploadfile',
     },
     SOCKET: {
         url: 'http://localhost:3000'
     }
 };
+
+// export const Configs = {
+//     USER: {
+//         register: 'https://nodejs-server-chat.herokuapp.com/' + 'api/user/resgister',
+//         login: 'https://nodejs-server-chat.herokuapp.com/' + 'api/auth/login',
+//         token: 'https://nodejs-server-chat.herokuapp.com/' + 'api/auth/getuserbytoken',
+//         update: 'https://nodejs-server-chat.herokuapp.com/' + 'api/user',
+//         uploadfile: 'https://nodejs-server-chat.herokuapp.com/' + 'api/user/uploadfile',
+//     },
+//     SOCKET: {
+//         url: 'http://localhost:3000'
+//     }
+// };
 
